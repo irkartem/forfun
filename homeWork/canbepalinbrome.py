cb2ef9b36695d023e0f38d3b6bc2aa52d4ae76a9
artem <artem@ispsystem.com>
2017-03-09 12:21:20 +0300

    Fun game

---
+++ homeWork/canbepalinbrome.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  DESCRIPTION:
+#       AUTHOR: artemirk@gmail.com ()
+# ===============================================================================
+def canpalindrome(str):
+
+    return True
+
+import unittest
+class Test(unittest.TestCase):
+
+      def test_pali(self):
+                self.assertTrue(canpalindrome('tactca'))
+
+if __name__ == '__main__':
+    unittest.main()
